6c8ca79dd9333d7cfe9ed7cdf12d8f116f6fdb22
hector diaz <hectordb19@gmail.com>
2023-03-18 19:19:01 -0300

    neuvo

--- src/proyecto_final/Amarre.java
+++ src/proyecto_final/Amarre.java
@@ -1,5 +1,39 @@
 package proyecto_final;
 
 public class Amarre {
+	private int numamarre;
+	private int lecturaagua;
+	private int lecturaluz;
+	
+	public Amarre(int numamarre, int lecturaagua, int lecturaluz) {
+		this.numamarre = numamarre;
+		this.lecturaagua = lecturaagua;
+		this.lecturaluz = lecturaluz;
+	}
 
+	public int getNumamarre() {
+		return numamarre;
+	}
+
+	public void setNumamarre(int numamarre) {
+		this.numamarre = numamarre;
+	}
+
+	public int getLecturaagua() {
+		return lecturaagua;
+	}
+
+	public void setLecturaagua(int lecturaagua) {
+		this.lecturaagua = lecturaagua;
+	}
+
+	public int getLecturaluz() {
+		return lecturaluz;
+	}
+
+	public void setLecturaluz(int lecturaluz) {
+		this.lecturaluz = lecturaluz;
+	}
+	
+	
 }
