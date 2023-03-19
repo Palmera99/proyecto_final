4bb463d87cd0f91068607052c1caa2a81866799b
hector diaz <hectordb19@gmail.com>
2023-03-19 17:29:46 -0300

    h

--- src/igu/Pantalla1.java
+++ src/igu/Pantalla1.java
@@ -5,10 +5,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
 
 public class Pantalla1 extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtNombre;
+	private JTextField txtRut;
+	private JTextField txtApellido;
+	private JTextField txtDireccion;
+	private JTextField txtTelefono;
+	private JTextField txtFechaIngreso;
 
 	/**
 	 * Launch the application.
@@ -31,11 +45,119 @@ public class Pantalla1 extends JFrame {
 	 */
 	public Pantalla1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 617, 542);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Ingresa un nombre");
+		
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ingreso de Socios");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel lblNewLabel_2 = new JLabel("Ingresa RUT");
+		
+		txtRut = new JTextField();
+		txtRut.setColumns(10);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("Ingresa apellido");
+		
+		txtApellido = new JTextField();
+		txtApellido.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Ingresa Direccion");
+		
+		txtDireccion = new JTextField();
+		txtDireccion.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Ingresa Telefono");
+		
+		txtTelefono = new JTextField();
+		txtTelefono.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Ingresa fecha de Ingreso");
+		
+		txtFechaIngreso = new JTextField();
+		txtFechaIngreso.setColumns(10);
+		
+		JButton btnSocios = new JButton("Guardar y Continuar");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+							.addComponent(txtRut, Alignment.LEADING)
+							.addComponent(txtNombre, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 258, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_2))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(41)
+							.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 406, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_4))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_5))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_2_1, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(lblNewLabel_3))
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+							.addComponent(txtFechaIngreso, Alignment.LEADING)
+							.addComponent(txtTelefono, Alignment.LEADING)
+							.addComponent(txtDireccion, Alignment.LEADING)
+							.addComponent(txtApellido, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 258, Short.MAX_VALUE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(66)
+							.addComponent(btnSocios)))
+					.addContainerGap(144, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(10)
+					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lblNewLabel_2)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtRut, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(14)
+					.addComponent(lblNewLabel)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_2_1)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_3)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(txtDireccion, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_4)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtTelefono, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblNewLabel_5)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(txtFechaIngreso, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(41)
+					.addComponent(btnSocios)
+					.addContainerGap(42, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
